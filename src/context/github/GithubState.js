0a60429df5ceab8b5d31cc01cbb3269242f971ac
Eduardo Romanini <eduroma@globo.com>
2020-04-19 19:34:49 -0300

    Preparing to production

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,6 +10,17 @@ import {
   GET_USER_REPOS,
 } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = (props) => {
   const initialState = {
     users: [],
@@ -26,8 +37,8 @@ const GithubState = (props) => {
 
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&
-        client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-        client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        client_id=${githubClientId}&
+        client_secret=${githubClientSecret}`
     );
 
     dispatch({ type: SEARCH_USERS, payload: res.data.items });
@@ -39,8 +50,8 @@ const GithubState = (props) => {
 
     const res = await axios.get(
       `https://api.github.com/users/${username}?
-        client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-        client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        client_id=${githubClientId}&
+        client_secret=${githubClientSecret}`
     );
 
     dispatch({ type: GET_USER, payload: res.data });
@@ -54,8 +65,8 @@ const GithubState = (props) => {
     setLoading();
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&
-        client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-        client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        client_id=${githubClientId}&
+        client_secret=${githubClientSecret}`
     );
     dispatch({ type: GET_USER_REPOS, payload: res.data });
   };
